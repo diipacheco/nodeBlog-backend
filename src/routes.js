edefdb60d134a232417ec53cc6c3fd743e25936f
unknown <edilsonpacheco6@gmail.com>
2019-05-09 19:15:07 -0300

    Cors established to the frontend can access the server HTTP

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const postController = require("./controllers/postController");
 const comentController = require("./controllers/comentController");
 
 routes.post("/post/:id", comentController.newComent);
-routes.post("/post/newpost", postController.newPost);
-routes.get("/post/:id", postController.show);
+routes.post("/newpost", postController.newPost);
+routes.get("/:id", postController.show);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 mongoose.connect("mongodb://192.168.99.100:27012/scienceComics", {
 	useNewUrlParser: true
